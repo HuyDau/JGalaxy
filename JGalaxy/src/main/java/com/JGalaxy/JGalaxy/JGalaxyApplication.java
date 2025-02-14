c579046538031b58de3caac38b20a69804e9c311
HuyDau <lehuydau2312@gmail.com>
2025-02-14 10:04:27 +0700

    Create New BE P

---
+++ JGalaxy/src/main/java/com/JGalaxy/JGalaxy/JGalaxyApplication.java
@@ -0,0 +1,13 @@
+package com.JGalaxy.JGalaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JGalaxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JGalaxyApplication.class, args);
+	}
+
+}
---
+++ JGalaxy/src/test/java/com/JGalaxy/JGalaxy/JGalaxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JGalaxy.JGalaxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JGalaxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
